2eed5f5daffce490faebb331eae479ba6824d7f3
Ariel Menendez <arielmenendez19@gmail.com>
2024-02-27 15:58:50 -0400

    binaryTree

---
+++ binaryTree.js
@@ -0,0 +1,13 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const root = new BinaryTree(1);
+root.left = new BinaryTree(2);
+root.right = new BinaryTree(3);
+
+console.log(root.left);
